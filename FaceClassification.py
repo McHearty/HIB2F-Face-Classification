8ca0ad63802208c83150f82c5ead57e8b49e7e5b
Jorden <McHearty@gmail.com>
2023-11-15 19:54:36 -0800

    Further cleanup

--- FaceClassification.py
+++ FaceClassification.py
@@ -6,14 +6,15 @@ from bpy.props import IntVectorProperty
 bl_info = {
     "name": "Face Classification",
     "author": "Calculating Contriverman",
-    "version": (1, 0, 0),
+    "version": (1, 0, 3),
     "blender": (3, 3, 1),
     "category": "Halo Infinite",
     "location": "View3D > Sidebar",
-    "description": "This version of the script will classify the faces of all selected mesh objects when the operator is executed. The total counts across all selected objects are accumulated and displayed in the panel.",
+    "description": "Classifies faces of selected mesh objects and displays statistics in the panel.",
 }
 
-class OT_FaceClassificationOperator(bpy.types.Operator):
+class FaceClassificationOperator(bpy.types.Operator):
+    """Classify Selected Objects Operator"""
     bl_idname = "object.face_classification_operator"
     bl_label = "Classify Selected Objects"
     bl_options = {'REGISTER', 'UNDO'}
@@ -25,66 +26,80 @@ class OT_FaceClassificationOperator(bpy.types.Operator):
         self.scalene_count = 0
 
     def execute(self, context):
-        mesh_objects_found = False
-        for obj in bpy.context.selected_objects:
-            mesh_objects_found = True
-            bpy.ops.object.mode_set(mode='OBJECT')
-            bm = bmesh.new()
-            bm.from_mesh(obj.data)
+        """Execute the classification process."""
+        try:
+            # Retrieve selected mesh objects
+            selected_objects = [obj for obj in bpy.context.selected_objects if obj.type == 'MESH']
+            
+            if not selected_objects:
+                self.report({'ERROR'}, "No mesh objects selected")
+                return {'CANCELLED'}
 
-            self.classify_triangles(bm.faces)
+            total_face_counts = [0, 0, 0, 0]  # Counters for isosceles, equilateral, right, scalene triangles
 
-            bm.free()
+            for obj in selected_objects:
+                bpy.ops.object.mode_set(mode='OBJECT')
+                bm = bmesh.new()
+                bm.from_mesh(obj.data)
 
-        if mesh_objects_found:
+                # Counters for each object
+                face_counts = [0, 0, 0, 0]
+
+                for face in bm.faces:
+                    if len(face.verts) == 3:
+                        # Process only triangular faces
+                        self._classify_triangle(face, face_counts)
+
+                bm.free()
+
+                # Accumulate counts
+                for i in range(len(total_face_counts)):
+                    total_face_counts[i] += face_counts[i]
+
             # Update the statistics
-            context.scene.face_classification_stats = [self.isosceles_count,
-                                                       self.equilateral_count,
-                                                       self.right_count,
-                                                       self.scalene_count]
-            status = {'FINISHED'}
+            context.scene.face_classification_stats = total_face_counts
+            return {'FINISHED'}
+        except Exception as e:
+            # Handle exceptions and report errors
+            self.report({'ERROR'}, f"An error occurred: {str(e)}")
+            return {'CANCELLED'}
 
+    def _classify_triangle(self, face, face_counts):
+        """Classify a triangular face based on angles and side lengths."""
+        # Calculate angles and classify a triangular face
+        vertices = [v.co for v in face.verts]
+
+        if len(vertices) != 3:
+            # Skip non-triangular faces
+            return
+
+        # Calculate the lengths of the sides
+        a = (vertices[1] - vertices[0]).length
+        b = (vertices[2] - vertices[1]).length
+        c = (vertices[0] - vertices[2]).length
+
+        # Calculate the angles using the Law of Cosines
+        alpha = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+        beta = math.degrees(math.acos((c**2 + a**2 - b**2) / (2 * c * a)))
+        gamma = math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
+
+        # Classify based on angles
+        if any(angle == 90 for angle in [alpha, beta, gamma]):
+            face_counts[2] += 1  # Right
+
+        # Classify based on side lengths
+        side_lengths = [a, b, c]
+        unique_lengths = len(set(side_lengths))
+
+        if unique_lengths == 1:
+            face_counts[1] += 1  # Equilateral
+        elif unique_lengths == 2:
+            face_counts[0] += 1  # Isosceles
         else:
-            self.report({'ERROR'}, "No mesh objects selected")
-            status = {'CANCELLED'}
+            face_counts[3] += 1  # Scalene
 
-        return status
-
-
-    def classify_triangles(self, bm_faces):
-        for face in bm_faces:
-            if len(face.verts) == 3:
-                vertices = [v.co for v in face.verts]
-
-                # Calculate the lengths of the sides
-                a = (vertices[1] - vertices[0]).length
-                b = (vertices[2] - vertices[1]).length
-                c = (vertices[0] - vertices[2]).length
-
-                # Calculate the angles using the Law of Cosines
-                alpha = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
-                beta = math.degrees(math.acos((c**2 + a**2 - b**2) / (2 * c * a)))
-                gamma = math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
-
-                # Classify based on angles
-                if 90 in [alpha, beta, gamma]:
-                    self.right_count += 1  # Right
-                    ### incr. count of right and possibly scalene / isosceles intended?
-                    ### if not add break here for a simple fix ###
-
-                # Classify based on side lengths
-                side_lengths = [a, b, c]
-                unique_lengths = len(set(side_lengths))
-
-                if unique_lengths == 1:
-                    self.equilateral_count += 1  # Equilateral
-                elif unique_lengths == 2:
-                    self.isosceles_count += 1  # Isosceles
-                else:
-                    self.scalene_count += 1  # Scalene
-
-
-class VIEW3D_PT_FaceClassificationMenu(bpy.types.Panel):
+class FaceClassificationPanel(bpy.types.Panel):
+    """Face Classification Panel"""
     bl_label = "Face Classification"
     bl_idname = "VIEW3D_PT_face_classification_menu"
     bl_space_type = 'VIEW_3D'
@@ -92,6 +107,7 @@ class VIEW3D_PT_FaceClassificationMenu(bpy.types.Panel):
     bl_category = 'Mesh to Forge Objects'
 
     def draw(self, context):
+        """Draw the user interface."""
         layout = self.layout
         stats = context.scene.face_classification_stats
 
@@ -100,7 +116,7 @@ class VIEW3D_PT_FaceClassificationMenu(bpy.types.Panel):
         if stats:
             face_types = ["Isosceles", "Equilateral", "Right", "Scalene"]
             for i, face_type in enumerate(face_types):
-                # Using "CHECKMARK" icon
+                # Use a checkmark icon for specific face types
                 icon_type = 'CHECKMARK' if face_type in ["Isosceles", "Equilateral", "Right"] else 'ERROR'
                 layout.label(text=f"{face_type}: {stats[i]}", icon=icon_type)
 
@@ -109,8 +125,9 @@ class VIEW3D_PT_FaceClassificationMenu(bpy.types.Panel):
             layout.label(text=f"Estimated Print: {estimated_print}", icon='FILE_IMAGE')
 
 def register():
-    bpy.utils.register_class(OT_FaceClassificationOperator)
-    bpy.utils.register_class(VIEW3D_PT_FaceClassificationMenu)
+    """Register the add-on."""
+    bpy.utils.register_class(FaceClassificationOperator)
+    bpy.utils.register_class(FaceClassificationPanel)
     bpy.types.Scene.face_classification_stats = IntVectorProperty(
         name="Face Classification Stats",
         description="Statistics of face classification",
@@ -118,8 +135,9 @@ def register():
     )
 
 def unregister():
-    bpy.utils.unregister_class(OT_FaceClassificationOperator)
-    bpy.utils.unregister_class(VIEW3D_PT_FaceClassificationMenu)
+    """Unregister the add-on."""
+    bpy.utils.unregister_class(FaceClassificationOperator)
+    bpy.utils.unregister_class(FaceClassificationPanel)
     del bpy.types.Scene.face_classification_stats
 
 if __name__ == "__main__":
