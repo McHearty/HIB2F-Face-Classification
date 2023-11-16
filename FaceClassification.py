f069310eba2bcf8ac18989adc0e7310ce4c26b49
Tim L. Chen <tim.li.chen@gmail.com>
2023-11-15 21:06:01 -0500

    untested refactoring

--- FaceClassification.py
+++ FaceClassification.py
@@ -18,65 +18,71 @@ class OT_FaceClassificationOperator(bpy.types.Operator):
     bl_label = "Classify Selected Objects"
     bl_options = {'REGISTER', 'UNDO'}
 
+    def __init__(self):
+        self.isosceles_count = 0
+        self.equilateral_count = 0
+        self.right_count = 0
+        self.scalene_count = 0
+
     def execute(self, context):
-        selected_objects = [obj for obj in bpy.context.selected_objects if obj.type == 'MESH']
-        
-        if not selected_objects:
-            self.report({'ERROR'}, "No mesh objects selected")
-            return {'CANCELLED'}
-
-        total_face_counts = [0, 0, 0, 0]  # isosceles, equilateral, right, scalene
-
-        for obj in selected_objects:
+        mesh_objects_found = False
+        for obj in bpy.context.selected_objects:
+            mesh_objects_found = True
             bpy.ops.object.mode_set(mode='OBJECT')
             bm = bmesh.new()
             bm.from_mesh(obj.data)
 
-            # Counters for each object
-            face_counts = [0, 0, 0, 0]
-
-            for face in bm.faces:
-                if len(face.verts) == 3:
-                    # Process only triangular faces
-                    self.classify_triangle(face, face_counts)
+            self.classify_triangles(bm.faces)
 
             bm.free()
 
-            # Accumulate counts
-            for i in range(len(total_face_counts)):
-                total_face_counts[i] += face_counts[i]
+        if mesh_objects_found:
+            # Update the statistics
+            context.scene.face_classification_stats = [self.isosceles_count,
+                                                       self.equilateral_count,
+                                                       self.right_count,
+                                                       self.scalene_count]
+            status = {'FINISHED'}
 
-        # Update the statistics
-        context.scene.face_classification_stats = total_face_counts
-        return {'FINISHED'}
-
-    def classify_triangle(self, face, face_counts):
-        vertices = [v.co for v in face.verts]
-
-        # Calculate the lengths of the sides
-        a = (vertices[1] - vertices[0]).length
-        b = (vertices[2] - vertices[1]).length
-        c = (vertices[0] - vertices[2]).length
-
-        # Calculate the angles using the Law of Cosines
-        alpha = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
-        beta = math.degrees(math.acos((c**2 + a**2 - b**2) / (2 * c * a)))
-        gamma = math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
-
-        # Classify based on angles
-        if any(angle == 90 for angle in [alpha, beta, gamma]):
-            face_counts[2] += 1  # Right
-
-        # Classify based on side lengths
-        side_lengths = [a, b, c]
-        unique_lengths = len(set(side_lengths))
-
-        if unique_lengths == 1:
-            face_counts[1] += 1  # Equilateral
-        elif unique_lengths == 2:
-            face_counts[0] += 1  # Isosceles
         else:
-            face_counts[3] += 1  # Scalene
+            self.report({'ERROR'}, "No mesh objects selected")
+            status = {'CANCELLED'}
+
+        return status
+
+
+    def classify_triangles(self, bm_faces):
+        for face in bm_faces:
+            if len(face.verts) == 3:
+                vertices = [v.co for v in face.verts]
+
+                # Calculate the lengths of the sides
+                a = (vertices[1] - vertices[0]).length
+                b = (vertices[2] - vertices[1]).length
+                c = (vertices[0] - vertices[2]).length
+
+                # Calculate the angles using the Law of Cosines
+                alpha = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+                beta = math.degrees(math.acos((c**2 + a**2 - b**2) / (2 * c * a)))
+                gamma = math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
+
+                # Classify based on angles
+                if 90 in [alpha, beta, gamma]:
+                    self.right_count += 1  # Right
+                    ### incr. count of right and possibly scalene / isosceles intended?
+                    ### if not add break here for a simple fix ###
+
+                # Classify based on side lengths
+                side_lengths = [a, b, c]
+                unique_lengths = len(set(side_lengths))
+
+                if unique_lengths == 1:
+                    self.equilateral_count += 1  # Equilateral
+                elif unique_lengths == 2:
+                    self.isosceles_count += 1  # Isosceles
+                else:
+                    self.scalene_count += 1  # Scalene
+
 
 class VIEW3D_PT_FaceClassificationMenu(bpy.types.Panel):
     bl_label = "Face Classification"
